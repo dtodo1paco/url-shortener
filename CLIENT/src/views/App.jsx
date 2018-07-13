d688688ed8fde9c934815cbdc042ca59fb4fdf20
dtodo1paco <dtodo1paco@gmail.com>
2018-07-13 18:44:51 +0200

    Refactor to remove router component (which was not really used)

--- CLIENT/src/views/App.jsx
+++ CLIENT/src/views/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
+
 import FooterDtodo1paco from 'views/FooterDtodo1paco.jsx';
 
 export default class App extends React.Component {
-  render() {
-    return (
-        <div>
-            {this.props.children}
-         <FooterDtodo1paco />
-        </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {this.props.children}
+                <FooterDtodo1paco />
+            </div>
+        )
+    }
 }
+

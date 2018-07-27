d2a7e70db367123b90bb70d2ac91426265206179
dtodo1paco <dtodo1paco@gmail.com>
2018-07-27 18:51:25 +0200

    update tests

--- CLIENT/tests/src/form.test.jsx
+++ CLIENT/tests/src/form.test.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import URLForm from 'views/URLForm.jsx';
+import URLForm from 'views/components/URLForm.jsx';
 import Button from "react-md/lib/Buttons"
 
 
-// https://github.com/airbnb/enzyme/issues/341
-// to get a default document where mount components
-import jsdom from 'jsdom'
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.document = doc
-global.window = doc.defaultView
-
 describe('URLForm component tests', function() {
-    it("click with empty value produces error", () => {
+
+    it("click with empty value shows error", () => {
         const wrapper = mount((<URLForm />));
         expect(wrapper.find('.md-text--error')).toHaveLength(0);
         const submit = wrapper.find(Button).first();
@@ -28,5 +22,15 @@ describe('URLForm component tests', function() {
         submit.simulate('click');
         expect(wrapper.find('label.md-text--error')).toHaveLength(0);
         expect(wrapper.find('.md-progress')).toHaveLength(1);
+    });
+    it("click with a valid value gets a transformed URL", () => {
+        const wrapper = mount((<URLForm />));
+        //wrapper.find('input').simulate('change', {target: {value: 'http://mylongurlwhichnobodywillremember.com'}});
+        wrapper.find('input').simulate('change', { target: { value: 'http://mylongurlwhichnobodywillremember.com' } })
+        const submit = wrapper.find(Button).first();
+        expect(submit.exists()).toBe(true)
+        submit.simulate('click');
+        expect(wrapper.find('label.md-text--error')).toHaveLength(0);
+        expect(wrapper.state('urlShortened')).toEqual('b70383d8');
     });
 });
--- CLIENT/tests/src/render.App.test.jsx
+++ CLIENT/tests/src/render.App.test.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+
 import MainApp from 'views/MainApp.jsx';
+import HomePage from 'views/pages/HomePage.jsx';
+import NotFound from 'views/pages/NotFound.jsx';
 
-// https://github.com/airbnb/enzyme/issues/341
-// to get a default document where mount components
-import jsdom from 'jsdom'
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.document = doc
-global.window = doc.defaultView
-
-describe('App renders correctly', function() {
-    it("renders the index page", () => {
-        const wrapper = mount(<MainApp />);
-        expect(wrapper.find('h2')).toHaveLength(1);
-        expect(wrapper.find('input')).toHaveLength(1);
-        expect(wrapper.find('button')).toHaveLength(1);
-        expect(wrapper.find('footer')).toHaveLength(1);
-    });
+describe('App render', function() {
+    it('Home path, routes to HomePage', () => {
+        const wrapper = mount(
+                <MainApp/>
+        );
+        expect(wrapper.find(HomePage)).toHaveLength(1);
+        expect(wrapper.find(NotFound)).toHaveLength(0);
+    })
 });
